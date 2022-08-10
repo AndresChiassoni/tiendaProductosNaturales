6e97f53e57d27192534d151ac2171ad4eda8932c
AndresChiassoni <andreschiassoni@gmail.com>
2022-08-10 19:33:20 -0300

    primera preentrega nuevas funciones

--- JS/main.js
+++ JS/main.js
@@ -4,8 +4,9 @@ const IVA = 1.21;
 let importeInicial = 5500;
 let precioFinal = 0;
 let cuotasFinac = 0;
+let acum = 0;
+let codigo = 1;
 const productos = [];
-
 
 class Producto {
     constructor(codigo, nombre, marca, precio, stock) {
@@ -55,7 +56,7 @@ function calculaIntereses(impInic, cuotas, tasaInt) {
 function agregarProductos() {
     //debugger
     let codigo = parseInt(prompt("Ingrese el código del producto a agregar: "))
-    let nombre = prompt("Ingrese el nombre del producto: ")
+    let nombre = (prompt("Ingrese el nombre del producto: ")).toUpperCase()
     let marca = (prompt("Ingrese la marca: ")).toUpperCase()
     let precio = parseFloat(prompt("Ingresa el importe"))
     let stock = parseInt(prompt("Ingrese el stock"))
@@ -63,24 +64,47 @@ function agregarProductos() {
     console.table(productos)
 }
 
-
 function generadorAutomatico() {
-    productos.push(new Producto(1221, 'Aceite de oliva extra virgen 500ml', 'TERRASANA', 732.15, 25));
-    productos.push(new Producto(1231, 'Salsa de tomate  500 ml sin T.A.C.C.', 'TERRASANA', 423.89, 30));
-    productos.push(new Producto(2554, 'Mermelada de sauco gr sin T.A.C.C.', 'TERRASANA', 529.22, 15));
-    productos.push(new Producto(3115, 'Vino orgánico Malbec roble 750 ml', 'TERRAVITA', 806.25, 12));
-    productos.push(new Producto(5255, 'Mayonesa vegana sabor original 352 gr', 'EGGLESS', 181.34, 50));
-    productos.push(new Producto(2711, 'Dátiles sin carozo x 200 grs pack compostable', 'PLANTY', 270.05, 50));
-    productos.push(new Producto(1544, 'Jugo y pulpa de arandano x 500 ml Sin TACC', 'NATUFRESH', 339.36, 25));
-    productos.push(new Producto(3555, 'Té Patagonia rooibos 15 Saq', 'INTI ZEN', 325.98, 10));
+    productos.push(new Producto(1221, 'ACEITE DE OLIVA EXTRA VIRGEN 500ML', 'TERRASANA', 732.15, 25));
+    productos.push(new Producto(1231, 'SALSA DE TOMATE  500 ML SIN T.A.C.C.', 'TERRASANA', 423.89, 30));
+    productos.push(new Producto(2554, 'MERMELADA DE SAUCO GR SIN T.A.C.C.', 'TERRASANA', 529.22, 15));
+    productos.push(new Producto(3115, 'VINO ORGÁNICO MALBEC ROBLE 750 ML', 'TERRAVITA', 806.25, 12));
+    productos.push(new Producto(5255, 'MAYONESA VEGANA SABOR ORIGINAL 352 GR', 'EGGLESS', 181.34, 50));
+    productos.push(new Producto(2711, 'DÁTILES SIN CAROZO X 200 GRS PACK COMPOSTABLE', 'PLANTY', 270.05, 50));
+    productos.push(new Producto(1544, 'JUGO Y PULPA DE ARANDANO X 500 ML SIN TACC', 'NATUFRESH', 339.36, 25));
+    productos.push(new Producto(3555, 'TÉ PATAGONIA ROOIBOS 15 SAQ', 'INTI ZEN', 325.98, 10));
+    productos.push(new Producto(3120, 'VINO ORGÁNICO CABERNET FRANC ROBLE 750 ML', 'TERRAVITA', 802.15, 12))
 }
 
+generadorAutomatico()
 
 
+function buscarProducto() {
+    //debugger
+    let product = (prompt("Ingresa el producto a buscar")).toUpperCase()
+    let resultado = productos.filter(elemento => elemento.nombre.includes(product))
+    console.table(resultado)
+}
 
+function calculaCompra() {
+    acum = 0;
+    do {
+        let codigo = parseInt(prompt("Ingresa el código del producto ('0' termina compra): "))
+        if (codigo == 0) {
+            break;
+        }
 
+        let cantidad = parseInt(prompt("Ingresa la cantidad"));
+        let resultado = productos.find(elemento => elemento.codigo === codigo);
+        if (resultado === undefined) {
+            alert("El codigo no existe!")
+        }
+        else {
+            console.log("Producto:", (resultado.nombre), " - precio unitario $", (resultado.precio),
+                "- cantidad:", cantidad, " subtotal $", (resultado.precio * cantidad).toFixed(2))
+            acum += (resultado.precio) * cantidad;
+        }
+    } while (codigo != 0);
 
-
-
-
-
+    console.log("Su compra TOTAL es de $", acum);
+}
